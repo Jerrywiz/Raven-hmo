8a53b4c52e4aa7968d820ec713f945579b118b17
Aimiton Jeremiah <jeremiahaimiton@gmail.com>
2023-04-16 01:56:09 +0100

    changes

--- src/Pages/Home/Header/Header.jsx
+++ src/Pages/Home/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
@@ -16,41 +16,65 @@ const Header = () => {
 
   return (
     <div className="head-bg">
-      <Navbar className="navbar navbar-expand-sm" collapseOnSelect expand="lg">
+      <Navbar className="navbar" expand="lg">
         <Container className="container-head">
-          <Navbar.Toggle onClick={toggleMenu} />
-          <Navbar.Collapse id="basic-navbar-nav" className={showMenu ? 'show' : ''}>
+          <Navbar.Toggle onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} />
+          </Navbar.Toggle>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className={showMenu ? "show" : ""}
+          >
             <Nav className="ms-auto align-items-center">
-              <Link to="/home" className="list-item text-decoration-none">
+              <Link
+                to="/home"
+                className="list-item text-decoration-none d-none d-lg-block"
+              >
                 Home
               </Link>
-              <Link to="/about" className="list-item text-decoration-none">
+              <Link
+                to="/about"
+                className="list-item text-decoration-none d-none d-lg-block"
+              >
                 About
               </Link>
-              <Link to="/service" className="list-item text-decoration-none">
+              <Link
+                to="/service"
+                className="list-item text-decoration-none d-none d-lg-block"
+              >
                 Service
               </Link>
-              <Link to="/dentist" className="list-item text-decoration-none">
+              <Link
+                to="/dentist"
+                className="list-item text-decoration-none d-none d-lg-block"
+              >
                 Dentist
               </Link>
-              <Link to="/contact" className="list-item text-decoration-none">
+              <Link
+                to="/contact"
+                className="list-item text-decoration-none d-none d-lg-block"
+              >
                 Contact
               </Link>
               {user.email ? (
                 <button
                   type="button"
-                  className="btn btn-danger"
+                  className="btn btn-danger d-none d-lg-block"
                   onClick={logout}
                 >
                   Log Out
                 </button>
               ) : (
-                <Link to="/login" type="button" className="btn btn-danger">
+                <Link
+                  to="/login"
+                  type="button"
+                  className="btn btn-danger d-none d-lg-block"
+                >
                   Login
                 </Link>
               )}
               {user.email && (
-                <Navbar.Text>
+                <Navbar.Text className="d-none d-lg-block">
                   <FontAwesomeIcon icon={faUser} />
                   <span className="userName">{user.displayName}</span>
                 </Navbar.Text>
